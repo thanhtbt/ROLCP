957ed5ee6ae3225534c9375cb4ebcd1091a70552
LE Trung-Thanh <letrungthanhtbt@gmail.com>
2021-01-31 23:03:48 +0100

    Add files via upload

---
+++ run_path.m
@@ -0,0 +1,15 @@
+% Add folders to path.
+addpath(pwd);
+
+cd Algorithms/;
+addpath(genpath(pwd));
+cd ..;
+
+cd Toolbox/;
+addpath(genpath(pwd));
+cd ..;
+
+cd Utilities/;
+addpath(genpath(pwd));
+cd ..;
+
